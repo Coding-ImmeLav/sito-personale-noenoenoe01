1d42ed247d3a07bbb625ca03832cd276b9b14b44
now <noemi.rubetti@edu.immaginazioneelavoro.it>
2025-03-27 10:39:19 +0100

    hofattoooo

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
+// creo una variabile e la metto uguale a 0 
 let a = 0; 
 
-function compariMenu () {
-    if (a == 0) {
-        
-        document.getElementById("menu").style.display ="inital";
-        a = 1
-
+// creo la funzione che fa comparire il menu o lo fa scomparire 
+// in base a quanto vale a 
+function compariMenu() {
+    if(a == 0) {
+        // se la a vale 0 succede quello che scrivo qui 
+        document.getElementById("menu").style.display = "initial";
+        a = 1;
     }
-       
-
-else if (a == 1) {
-    document.getElementById("menu").style.display = "none";
-    a = 0;
-}
-     
+    else if(a == 1) {
+        document.getElementById("menu").style.display = "none";
+        a = 0;
+    }
 }
